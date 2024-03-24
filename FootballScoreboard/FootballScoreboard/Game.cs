9b140a7463188782311b65919cebb70cc057c39a
crowalpaca <gn.dennis.savin@gmail.com>
2024-03-24 17:33:18 +0100

    Added the database.

---
+++ FootballScoreboard/FootballScoreboard/Game.cs
@@ -0,0 +1,11 @@
+﻿namespace FootballScoreboard
+{
+	public struct Game
+	{
+		public string HomeTeam;
+		public string AwayTeam;
+		public int HomeTeamScore;
+		public int AwayTeamScore;
+		public DateTime StartTime;
+	}
+}
