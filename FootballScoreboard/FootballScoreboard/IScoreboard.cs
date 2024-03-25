52c687c60c73c280a4137318d4385887983d242b
crowalpaca <gn.dennis.savin@gmail.com>
2024-03-25 02:18:05 +0100

    Updated comments.

--- FootballScoreboard/FootballScoreboard/IScoreboard.cs
+++ FootballScoreboard/FootballScoreboard/IScoreboard.cs
@@ -1,50 +1,26 @@
 ﻿namespace FootballScoreboard
 {
-	public class Scoreboard
+	public interface IScoreboard
 	{
-		private readonly IDatabase _database;
+		/// <summary>
+		/// Adds a new game between home team and away team with default scores of 0 - 0 to the scoreboard.
+		/// </summary>
+		void StartGame(string homeTeam, string awayTeam);
 
-		public Scoreboard(IDatabase database)
-		{
-			_database = database;
-		}
+		/// <summary>
+		/// Updates an existing game with a new pair of absolute scores.
+		/// </summary>
+		void UpdateGame(string homeTeam, string awayTeam, int homeTeamScore, int awayTeamScore);
 
-		public void StartGame(string homeTeam, string awayTeam)
-		{
-			var game = new Game()
-			{
-				HomeTeam = homeTeam,
-				AwayTeam = awayTeam,
-				StartTime = DateTime.Now,
-			};
-
-			_database.SetGame(game);
-		}
-
-		public void UpdateGame(string homeTeam, string awayTeam, int homeTeamScore, int awayTeamScore)
-		{
-			var game = _database.GetGame(homeTeam, awayTeam);
-			game.HomeTeamScore = homeTeamScore;
-			game.AwayTeamScore = awayTeamScore;
-			
-			_database.SetGame(game);
-		}
-
-		public void FinishGame(string homeTeam, string awayTeam)
-		{
-			_database.RemoveGame(homeTeam, awayTeam);
-		}
+		/// <summary>
+		/// Removes the game from the scoreboard.
+		/// </summary>
+		void FinishGame(string homeTeam, string awayTeam);
 
 		/// <summary>
 		/// Returns a summary of games in progress ordered by their total score. 
 		/// The games with the same total score will be returned ordered by the most recently started match in the scoreboard.
 		/// </summary>
-		public Game[] GetOrderedGames()
-		{
-			var games = from game in _database.Games 
-				orderby (game.HomeTeamScore + game.AwayTeamScore) descending, game.StartTime ascending select game;
-
-			return games.ToArray();
-		}
+		Game[] GetOrderedGames();
 	}
 }
